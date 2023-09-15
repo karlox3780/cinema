2406bf317aa6db12823c05a1c006b4b293ae27ef
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-09-15 21:29:05 +0200

    Añadida nueva llamada al movies service

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -22,12 +22,15 @@ export class MoviesService {
         return this.http.get<any>(`https://api.themoviedb.org/3/movie/popular?language=es-ES&api_key=${this.apikey}`)
     }
     getCreditsMovie(movie_id: number) {
-        return this.http.get<any>(`https://api.themoviedb.org/3/movie/` + movie_id + `/credits?language=es-ES&api_key=${this.apikey}`)
+        return this.http.get<any>(`https://api.themoviedb.org/3/movie/${movie_id}/credits?language=es-ES&api_key=${this.apikey}`)
     }
     getMovieDetails(movie_id: number) {
-        return this.http.get<any>(`https://api.themoviedb.org/3/movie/` + movie_id + `?language=es-ES&api_key=${this.apikey}`)
+        return this.http.get<any>(`https://api.themoviedb.org/3/movie/${movie_id}?language=es-ES&api_key=${this.apikey}`)
     }
     getMovieSearch(movie_name: string, page: number) {
-        return this.http.get<any>(`https://api.themoviedb.org/3/search/movie?query=` + movie_name + `&include_adult=false&language=es-ES&page=` + page + `&api_key=${this.apikey}`)
+        return this.http.get<any>(`https://api.themoviedb.org/3/search/movie?query=${movie_name}&include_adult=false&language=es-ES&page=` + page + `&api_key=${this.apikey}`)
+    }
+    getPersonDetails(person_id: string) {
+        return this.http.get<any>(`https://api.themoviedb.org/3/person/${person_id}?language=es-ES&api_key=${this.apikey}`)
     }
 }
